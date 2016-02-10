39b6de5f11690c392816b79969593ae3f497779d
authorbeard <msnolan@gmail.com>
2016-02-09 19:38:00 -0500

    all tests passing

---
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -0,0 +1,11 @@
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
+
+  def change 
+    change_table :students do |t|
+      t.integer :grade 
+      t.string :birthdate
+    end
+
+  end
+
+end
