18082f4fa6aec1f2f4cd4bd47218e699ed2c1823
jjlippman <jjlippman@earthlink.net>
2016-03-03 13:49:17 -0700

    Done.

---
+++ db/migrate/01_create_students.rb
@@ -0,0 +1,21 @@
+class CreateStudents < ActiveRecord::Migration
+  def up
+  end
+ 
+  def down
+  end
+
+  def change
+    create_table :students do |t|
+      t.string :name
+     end
+  end
+
+
+
+
+end
+
+
+
+
