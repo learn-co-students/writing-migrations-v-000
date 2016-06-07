97e86ac558fa00124a7752bf529e02b27983e64c
jwolfe890 <jwolfe890@gmail.com>
2016-06-07 00:54:32 -0400

    alldone

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,2 +1,9 @@
+# db/migrate/03_change_datatype_for_birthdate.rb
+
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration
+
+  def change
+    change_column :students, :birthdate, :datetime 
+  end
+
 end
