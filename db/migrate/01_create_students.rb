8353bf24d24472da57b5f93530f61e5725c4ed15
Jennifer Dixon <jenniration@gmail.com>
2016-07-27 02:16:32 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,18 @@
 class CreateStudents < ActiveRecord::Migration
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXITS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT
+      )
+    SQL
+
+    ActiveRecord::Base.connection.execute(sql)
+  end
+
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
 end
