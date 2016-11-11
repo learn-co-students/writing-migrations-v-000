9298f73d53d480cf15075a995cdab357485235c8
William Story <wstory@villanova.edu>
2016-11-11 06:58:01 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,13 @@
 class Student < ActiveRecord::Base
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+      )
+    SQL
+
+    ActiveRecord::Base.connection.execute(sql)
+  end
 end
