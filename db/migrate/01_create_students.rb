0e79a637f335b3f211ccb9f4120a20c131611ac9
Leonardo Guzman <lguzman@me.com>
2017-07-18 21:29:10 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,9 @@
 class CreateStudents < ActiveRecord::Migration
+  #create table using active record
+  def change
+    #need to name table by using :students (symbol)
+    create_table :students do |t|
+      t.string :name
+    end
+  end
 end
