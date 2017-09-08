58ff5edbcdc58c6114a57c7035f050f49c7c19fe
Elvin <Dimix181@Gmail.com>
2017-09-07 23:31:31 -0400

    all test are passing

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,23 @@
-class CreateStudents < ActiveRecord::Migration
+class CreateStudents < ActiveRecord::Migration[5.1]
+
+#  connection = ActiveRecord::Base.establish_connection(
+#    :adapter =>  "sqlite3",
+#    :database => "db/students.sqlite"
+#  )
+
+#  sql = <<-SQL
+#        CREATE TABLE IF NOT EXISTS students(
+#          id INTEGER PRIMARY KEY,
+#          name 'TEXT'
+#        )
+#        SQL
+
+#    ActiveRecord::Base.connection.execute(sql)
+
+  def change
+    create_table :students do |t|
+        t.string :name
+    end
+  end
+
 end
