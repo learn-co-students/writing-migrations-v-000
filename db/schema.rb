ea82ce22704521862a40ff2a3a92e589b6c04165
Yujun Sung <yujunsung@gmail.com>
2017-10-24 02:38:07 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 1) do
   create_table "students", force: :cascade do |t|
     t.string   "name"
     t.integer  "grade"
