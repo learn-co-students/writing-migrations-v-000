7b2b3546ea114cfaebd9da0ca6c1324dd385eaa8
roitblatari <roitblatari@gmail.com>
2017-11-23 20:53:44 -0500

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,9 +1,7 @@
 class CreateStudents < ActiveRecord::Migration
   def change
-    # create_table :students do |t|
-    #   t.string :name
-    #   t.string :genre
-    #   t.integer :age
-    #   t.string :hometown
-    # end
+    create_table :students do |t|
+      t.string :name
+    end
+  end
 end
