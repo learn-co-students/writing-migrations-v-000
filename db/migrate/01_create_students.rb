fdc4ce44357f417ae18e8b86b0d5bd5d1f9ff15f
Olena Ageyeva <eageeva@hotmail.com>
2017-12-04 13:42:13 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -8,7 +8,7 @@ class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
      t.string :name
-     t.integer :grade
+     #t.integer :grade
     end
   end
 end
