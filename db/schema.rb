5839023dfb28af8cbeded7d0f9f5d3b2195802df
ruthmesfun <ruth.mesfun@gmail.com>
2018-02-16 14:19:23 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -14,8 +13,8 @@
 ActiveRecord::Schema.define(version: 3) do
 
   create_table "students", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "grade"
+    t.string "name"
+    t.integer "grade"
     t.datetime "birthdate"
   end
 
