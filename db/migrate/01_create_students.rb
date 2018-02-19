d6772c7895c6fe8b85360508ac23a4bfe06ede2e
suzy baldwin <gbaldwin24@comcast.net>
2018-02-19 19:30:40 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,15 @@
 class CreateStudents < ActiveRecord::Migration
+
+
+def change
+
+  create_table :students do |t|
+    t.string :name
+    t.string :grade
+    t.string :birthdate
+  end
+
+end
+
+
 end
