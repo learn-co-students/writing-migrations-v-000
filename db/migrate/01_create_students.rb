6521955f0cb3288404e5e6cb8623d2ff056987f4
Wali89 <wali.rahman89@yahoo.com>
2018-04-02 06:45:35 -0400

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,14 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :students do |t|
+      t.string :name
+    end 
+  end 
+
+  def up
+  end
+
+  def down
+  end
+
 end
