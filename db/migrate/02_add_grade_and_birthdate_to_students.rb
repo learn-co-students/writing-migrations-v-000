ff5bcde7e0ed5590eda41bf8812af2f18e669405
Joanne Lee <vernapii@gmail.com>
2018-04-24 17:41:36 +0000

    Done.

---
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -0,0 +1,8 @@
+class AddGradeAndBirthdayToStudents < ActiveRecord::Migration[4.2]
+  def change
+    add_column :students do |t|
+      t.integer :grade
+      t.string :birthdate
+    end
+  end
+end
---
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -0,0 +1,5 @@
+class ChangeDataypeForBirthdate < ActiveRecord:Migration[4.2]
+  def change
+    change_column(table_name, column_name, type)
+  end
+end
