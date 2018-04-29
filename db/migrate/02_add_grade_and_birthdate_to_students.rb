684ae694d7266b5768c2d2f5e4a2b89543b1a7e9
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-04-29 14:41:50 +0000

    Done.

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,8 +1,9 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
 
   def change
-    create_table :students do |t|
-      t.string :name
+    add_column :students do |t|
+      t.integer :grade
+      t.string :birthdate
     end
   end
 end
