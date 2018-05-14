32edf09fc47b0c283b8d942675fbeaf2e848eb26
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-14 00:10:30 +0000

    Done.

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
-class AddGradeAndBirthday < ActiveRecord::Migration[4.2]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[4.2]
 
   def change
     add_column :students, :grade, :integer
--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
-class ChangeDataTypeForBirthdate < ActiveRecord::Migration[4.2]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[4.2]
   def change
-    change_column(students, birthdate, datetime)
+    change_column(:students, :birthdate, :datetime)
   end
 
 end
