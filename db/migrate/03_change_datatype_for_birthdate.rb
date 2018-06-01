d34bebeb5152bf06550ced4e014971e9d878f128
Fiona Lapham <fionadlapham@gmail.com>
2018-06-01 21:40:13 +0000

    Done.

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,5 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def change
-    change_column :students do |t|
-      t.datetime :birthdate
-    end
+    change_column :students, :birthdate, :datetime
   end
 end
