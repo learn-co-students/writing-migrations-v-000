026b33533cd2a655c048067cb0985779aba981f4
Fiona Lapham <fionadlapham@gmail.com>
2018-06-01 21:06:19 +0000

    Done.

---
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -0,0 +1,10 @@
+
+
+class AddGradeAndBirthdayToStudents < ActiveRecord::Migration[5.1]
+  def change
+    add_column :students do |t|
+      t.integer :grade
+      t.string :birthdate
+    end
+  end
+end
