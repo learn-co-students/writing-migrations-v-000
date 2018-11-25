8ccef6a3a08e4c947b9740fdb582375761577e9b
Rachel Wyatt <rw126@evansville.edu>
2018-11-25 15:58:41 +0000

    Done.

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
-class AddGradeandBirthdatetoStudents < ActiveRecord::Migration
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[4.2]
   def change
     add_column :students, :grade, :integer
     add_column :students, :birthdate, :string
