9c5d3a9d641c38c99d97bb1217cc02eb938573af
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-05 19:11:57 +0000

    Done.

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,8 +1,7 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   
   def change(table_name= nil, column_name =nil, type=nil)
-    change_column :grade, :integer
-    change_column :birthdate, :datetime
+    change_column :students, :birthdate, :datetime
   end
   
 end
