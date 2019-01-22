0857bf4bab7f1b942d5b3d242f5d28c4dfa8a334
Joann Pan <joannlpan@gmail.com>
2019-01-22 21:41:32 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,9 @@
 ActiveRecord::Schema.define(version: 1) do
 
   create_table "students", force: :cascade do |t|
-    t.string "name"
+    t.string   "name"
+    t.integer  "grade"
+    t.datetime "birthdate"
   end
 
 end
