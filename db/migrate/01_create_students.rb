b9855260774f03e313f34e83a7534839b9c9c99e
Tim Knoll <timothy.knoll@gmail.com>
2019-07-28 03:01:50 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,9 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+
+  def change #creating a table and student + name
+    create_table :students do |t|
+      t.string :name
+    end   
+  end   
+  
 end
