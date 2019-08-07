3c5b22b8d8df322f9ad6eb675a319c8117dca555
jontenthoff <jontenthoff@hotmail.com>
2019-08-07 19:39:05 +0000

    Done.

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,8 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
-  def change 
-    add_column :students do |t|
-      t.integer :age 
-      t.string :birthdate
-  end 
-  end
+  def change
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
+    end
 end
--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,10 +1,6 @@
-class ChangeDataTypeForBirthdate < ActiveRecord::Migration[5.2]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2]
   def change 
-    change_column(table_name, column_name, type)
-      change_column :students do |t|
-        t.string :name 
-        t.integer :age 
-        t.datetime :birthdate 
-      end
-  end 
+    change_column :students, :birthdate, :datetime
+    
+    end 
 end 
