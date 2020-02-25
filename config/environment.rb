18a8efff8fc560b52514655f03bdc5c1feed19ee
alejandra cantu <alejandra.cantu13@gmail.com>
2020-02-25 21:36:43 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 require 'bundler/setup'
 Bundler.require
 
+
+
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
 
