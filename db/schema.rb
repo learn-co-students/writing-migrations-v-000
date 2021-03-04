64715492b94458e6b7ca7ed2f0ba9f9391d376cd
Vicky  Tee <vttak9@gmail.com>
2021-03-04 21:49:30 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,10 @@
-# This file is auto-generated from the current state of the database. Instead
+ActiveRecord::Schema.define(version: 1) do
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+  end
+
+end# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
